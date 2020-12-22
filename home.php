700d9131d5fbee3fcd555e6a6c055af8e5d7c805
Manon5 <manon.touba05@gmail.com>
2020-12-22 14:33:35 +0100

    inscription dans la bdd

---
+++ home.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+  <title>Accueil</title>
+</head>
+
+<body>
+  <form>
+  <button formaction="inscription.php">Créer un compte</button>
+  <button formaction="connexion.php">Se connecter</button>
+</form>
+  <p>Bonjour</p>
+</body>
+</html>
---
+++ inscription.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+  <title>Créer un compte</title>
+</head>
+
+<body>
+  <form action="script_inscription.php" method="POST">
+    Login : <input type="text" name="login"/><br/>
+    Mot de passe : <input type="password" name="password"/><br/>
+    Prénom : <input type="text" name="prenom"/><br/>
+    Nom : <input type="text" name="nom"/> <br/>
+    Sexe : Homme<input type="radio" name="sexe" value="homme"/> Femme<input type="radio" name="sexe" value="femme"/><br/>
+    Mail : <input type="text" name="mail"/> <br/>
+    Date de naissance : <input type="date" name="date_nais"/> <br/>
+    Adresse : <input type="text" name="adresse"/> <br/>
+    Ville : <input type="text" name="ville"/> <br/>
+    Code postal : <input type="text" name="cp"/> <br/>
+    Téléphone : <input type="text" name="tel"/> <br/>
+    <input type="submit" value="Valider"/>
+  </form>
+</body>
+</html>
