85db022ed2352dccf7faeab85600e61db929b2fa
Manon5 <manon.touba05@gmail.com>
2020-12-22 14:54:25 +0100

    Update script_inscription.php

--- script_inscription.php
+++ script_inscription.php
@@ -1,5 +1,5 @@
 <?php
-$db = mysqli_connect("127.0.0.1:3306", "root", "Bemuwu_5", "proj_dev_web");
+$db = mysqli_connect("127.0.0.1:3306", "root", "mdp", "proj_dev_web");
 $query = "INSERT INTO user (login, mdp, prenom, nom, sexe, mail, date_nais, adresse, ville, code_postal, telephone) VALUES ('{$_POST["login"]}', '{$_POST["password"]}', '{$_POST["prenom"]}', '{$_POST["nom"]}', '{$_POST["sexe"]}', '{$_POST["mail"]}', '{$_POST["date_nais"]}', '{$_POST["adresse"]}', '{$_POST["ville"]}', '{$_POST["cp"]}', '{$_POST["tel"]}');";
 $res = mysqli_query($db, $query);
 echo $query;
